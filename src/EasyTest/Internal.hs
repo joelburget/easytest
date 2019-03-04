33147549a8c2863a98f180a667ec540844933756
Joel Burget <joelburget@gmail.com>
2019-03-04 08:28:35 -0800

    Remove some unused (?) pragmas.

--- src/EasyTest/Internal.hs
+++ src/EasyTest/Internal.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs             #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Rank2Types        #-}
